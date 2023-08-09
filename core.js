808f3e1dd3d92d9d6678db55b9744b763736b625
Robert Allen <contact@robertdallen.com>
2023-08-08 22:41:02 -0400

    Sentence and chunk rewrite
    Total rewrite of how chunks are made, we break up sentences first now, then chunks. Regex treats numbers with decimals as strings. Punctuation is correctly retained.

--- core.js
+++ core.js
@@ -76,13 +76,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     let chunk = words.slice(0, chunkEnd);
     let chunkText = chunk.join(' ') + ' ';
 
-    // If the selected font family is "ADHD", highlight the first two letters
-    if (this.value === 'ADHD') {
-        chunkText = highlightFirstTwoLetters(chunkText);
-    }
+    console.log(chunk);
 
     // Set the first chunk as the text output
     textOutput.innerHTML = chunkText;
+
+    console.log(textOutput.innerHTML);  // Ensure this logs the correct text.
+     
   });
 
   startPauseButton.addEventListener('click', function() {
@@ -96,49 +96,111 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
   });
 
-  function highlightFirstTwoLetters(text) {
-    return text.replace(/\b(\w{1,2})(\w*)\b/g, '&nbsp;<span class="highlight">$1</span>$2');
+//-------------------------------------
+//-------------------------------------
+// START of splitIntoSentences function
+function splitIntoSentences(text) {
+  let splitParts = text.split(/(?<!\d)\.(?!\d)(?=\s|$|\s?[A-Z])|([!?])\s+/);
+  let sentences = [];
+
+  for (let i = 0; i < splitParts.length; i += 2) {
+      let sentence = splitParts[i];
+      let punctuation = splitParts[i + 1] || '.'; // Default to a period if no punctuation captured
+      sentences.push(sentence.trim() + punctuation);
   }
 
-  function startReading() {
-    let text = textInput.value;
-    let words = text.split(/\s+/);
-    let chunkSize = parseInt(chunkSelector.value);
-    let i = 0;
+  return sentences;
+}
 
-    function nextChunk() {
-        if (i >= words.length) {
-            startPauseButton.innerHTML = 'Start';
-            isReading = false;
-            return;
-        }
+// END of splitIntoSentences function
 
-        let chunkEnd = Math.min(i + chunkSize, words.length);
-        let chunk = words.slice(i, chunkEnd);
-        let chunkText = chunk.join(' ');
+// START of getChunksFromSentences function
+function getChunksFromSentences(sentences, chunkSize) {
+  let allWords = [];
+  sentences.forEach(sentence => {
+      let wordsInSentence = sentence.split(/\s+/);
+      allWords.push(...wordsInSentence);
+  });
 
-        // This regex matches sentence-ending punctuation, numbers, URLs, and paragraph breaks
-        const specialCharacterRegex = /(\d+(\.\d+)?|[.!?]|https?:\/\/[^\s]+|\s{2,})/g;
-        let delay = 60000 / parseInt(speedSelector.value);
+  let i = 0;
+  let chunks = [];
+  while (i < allWords.length) {
+      let chunkEnd = i + chunkSize;
 
-        // If the chunk contains a special character, add an extra delay
-        if (specialCharacterRegex.test(chunkText)) {
-          delay += 60000 / parseInt(speedSelector.value) * parseFloat(pauseSpeedSelector.value);// add a delay relative to reading speed
-        }
+      if (/[.!?]$/.test(allWords[chunkEnd - 1])) {
+          chunkEnd = chunkEnd;
+      } else {
+          let nextPunctuationIndex = allWords.slice(i).findIndex(word => /[.!?]$/.test(word));
+          if (nextPunctuationIndex !== -1 && nextPunctuationIndex < chunkSize) {
+              chunkEnd = i + nextPunctuationIndex + 1;
+          }
+      }
 
-        if (fontFamilySelector.value === 'ADHD') {
-            chunkText = highlightFirstTwoLetters(chunkText);
-        }
+      let chunk = allWords.slice(i, chunkEnd);
+      chunks.push(chunk);
 
-        textOutput.innerHTML = chunkText;
-
-        i += chunkSize;
-
-        if (isReading) {
-            setTimeout(nextChunk, delay);
-        }
-    }
-
-    nextChunk();
+      i += chunk.length;
   }
+  
+  return chunks;
+}
+// END of getChunksFromSentences function
+//-------------------------------------
+//-------------------------------------
+
+
+
+function startReading() {
+  let sentences = splitIntoSentences(textInput.value);
+  let chunks = getChunksFromSentences(sentences, parseInt(chunkSelector.value));
+  let currentChunkIndex = 0;
+
+  function nextChunk() {
+      // Check if all chunks are processed
+      if (currentChunkIndex >= chunks.length) {
+          startPauseButton.innerHTML = 'Start';
+          isReading = false;
+          return;
+      }
+
+      let chunk = chunks[currentChunkIndex];
+      currentChunkIndex++;  // Move to the next chunk for the next cycle
+
+      let chunkText = chunk.join(' ');
+
+      // This regex matches sentence-ending punctuation, numbers, URLs, and paragraph breaks
+      const specialCharacterRegex = /(\d+(\.\d+)?|[.!?]|https?:\/\/[^\s]+|\s{2,})/g;
+      let delay = 60000 / parseInt(speedSelector.value);
+
+      // If the chunk contains a special character, add an extra delay
+      if (specialCharacterRegex.test(chunkText)) {
+          delay += 60000 / parseInt(speedSelector.value) * parseFloat(pauseSpeedSelector.value); // add a delay relative to reading speed
+      }
+
+      // If the selected font family is "ADHD", highlight the first two letters
+      if (fontFamilySelector.value === 'ADHD') {
+          chunkText = highlightFirstTwoLetters(chunkText);
+      }
+      function highlightFirstTwoLetters(text) {
+          return text.replace(/\b(\w{1,2})(\w*)\b/g, '&nbsp;<span class="highlight">$1</span>$2');
+      }
+
+      textOutput.innerHTML = chunkText;
+
+      if (isReading) {
+          setTimeout(nextChunk, delay);
+      }
+
+      console.log("Current chunk:", chunk);
+  }
+
+  nextChunk();
+
+  console.log("Sentences array:", sentences);
+  console.log("Chunks array:", chunks);
+}
+
+
+
+
 });
